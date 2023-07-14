e21cc13252a57510f96febaa5629a4efb04ca53b
Harshdeep2611 <73888870+Harshdeep2611@users.noreply.github.com>
2023-07-15 02:40:48 +0530

    index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $insert = false;
 if(isset($_POST['name'])){
-    // Set connection variables
+   
     $server = "localhost";
     $username = "root";
     $password = "";
 
-    // Create a database connection
+   
     $con = mysqli_connect($server, $username, $password);
 
     // Check for connection success
@@ -46,7 +46,7 @@ if(isset($_POST['name'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Travel Form</title>
+    <title>Welcome to Industrial Visit Form</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
